5cb3754c6bcf9dd046538846c21dcbf56ef6d659
JoeyMCPC <142174364+JoeyMCPC@users.noreply.github.com>
2024-06-27 12:05:00 -0400

    Update auth_core.h
    chatgpt fixed stuff

--- auth_core.h
+++ auth_core.h
@@ -1,73 +1,56 @@
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
 
 /*
-
 
 Authentication Core V1
 
 This authentication program (very bad) is to very lightly secure the more important aspects of a server
 
-
 */
 
-
-
-int num_of_attempts_login;
-bool didLogin;
-
-bool hasAccess;
-
+int num_of_attempts_login = 0;
+bool didLogin = false;
+bool hasAccess = false;
 
 int password = 1234;
 
-
-
-bool doLogin(password, didLogin, num_of_attempts_login)
+bool doLogin(int password, bool &didLogin, int &num_of_attempts_login)
 {
-	int guessed_password;
-	std::cout << "enter password: ";
-	cin >> guessed_password;
+    int guessed_password;
+    std::cout << "Enter password: ";
+    std::cin >> guessed_password;
 
-	if(password = guessed_password)
-	{
-		didLogin = true;
-		return didLogin;
-	}
-	else
-	{
-		didLogin = false;
-		num_of_attempts_login = num_of_attempts_login +1;
-		return didLogin;
-	}
+    if (password == guessed_password)
+    {
+        didLogin = true;
+        return didLogin;
+    }
+    else
+    {
+        didLogin = false;
+        num_of_attempts_login = num_of_attempts_login + 1;
+        return didLogin;
+    }
 }
 
-
-void onAttempt(num_of_attempts_login, didLogin)
+void onAttempt(int num_of_attempts_login, bool didLogin)
 {
-	if (num_of_attempts_login > 3)
-	{
-		std::cout << "You have been locked out of this program";
-		exit()
-
-	}
-	if (num_of_attempts_login < 3)
-	{
-		if(didLogin == true)
-		{
-		// call give access program
-		hasAccess = true;
-
-		}
-		else 
-		{
-			std::cout << "You have been locked out of this program";
-			exit()
-		}
-	}
+    if (num_of_attempts_login > 3)
+    {
+        std::cout << "You have been locked out of this program";
+        std::exit(EXIT_FAILURE);
+    }
+    else
+    {
+        if (didLogin)
+        {
+            hasAccess = true;
+        }
+        else
+        {
+            std::cout << "You have been locked out of this program";
+            std::exit(EXIT_FAILURE);
+        }
+    }
 }
-
-
-
-
-
