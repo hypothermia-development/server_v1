993d1f9f365c93f2df5c1f557d2a7f62b5816290
JoeyMCPC <142174364+JoeyMCPC@users.noreply.github.com>
2024-06-27 12:05:51 -0400

    Update file_management.h
    chatgpt fixed and added some stuff

--- file_management.h
+++ file_management.h
@@ -2,31 +2,60 @@
 #include <fstream>
 #include "auth_core.h"
 #include <cstdio>
+#include <string>  // Include the string header
 
 /*
 
 todo:
 
-create_file function
-read_file function
-authentication implementation
-list files
-create folders
-change directories 
+create_file function - done by chatgpt
+read_file function - done by chatgpt
+authentication implementation - done by me
+list files- todo the 28th	
+create folders - todo the 28th
+change directories todo the 30th
 be able to open program into folders (i.e starting directory)
 
 */
 
-void delete_file()
+void delete_file(const std::string &file_name)
 {
-	remove(file_name);
-	std::cout << "The respective file has been deleted.";
+    std::remove(file_name.c_str());
+    std::cout << "The respective file has been deleted." << std::endl;
 }
 
-int create_file()
+bool create_file(const std::string &file_name)
 {
-
+    std::ofstream file(file_name);
+    if (file)
+    {
+        std::cout << "File created successfully: " << file_name << std::endl;
+        file.close();
+        return true;
+    }
+    else
+    {
+        std::cerr << "Failed to create file: " << file_name << std::endl;
+        return false;
+    }
 }
 
-
-
+bool read_file(const std::string &file_name)
+{
+    std::ifstream file(file_name);
+    if (file)
+    {
+        std::string line;
+        while (std::getline(file, line))
+        {
+            std::cout << line << std::endl;
+        }
+        file.close();
+        return true;
+    }
+    else
+    {
+        std::cerr << "Failed to open file: " << file_name << std::endl;
+        return false;
+    }
+}
