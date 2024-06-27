91af147257ad8c9cb35aac85f51400e65ceef208
JoeyMCPC <142174364+JoeyMCPC@users.noreply.github.com>
2024-06-27 12:05:25 -0400

    Update file_management.cpp
    chatgpt added and fixed some stuff

--- file_management.cpp
+++ file_management.cpp
@@ -1,49 +1,54 @@
 #include "auth_core.h"
 #include "file_mangement.h"
-
-
-
+#include <iostream>
+#include <string>
 
 int file_manager()
 {
+    // Authentication loop v1
+    while (true)
+    {
+        if (hasAccess == true)
+        {
+            break;
+        }
 
-	// authentication loop v1
-	while (true)
-	{
-		if (hasAccess = true)
-		{
-		break;
-		}
-		
+        doLogin(password, didLogin, num_of_attempts_login);
+        onAttempt(num_of_attempts_login, didLogin);
+    }
 
-		doLogin(password, didLogin, num_of_attempts_login);
-		onAttempt(num_of_attempts_login, didLogin)
-	}
+    while (true)
+    {
+        std::string command;
+        std::cout << "Enter Command: ";
+        std::cin >> command;
 
+        if (command == "delete file")
+        {
+            std::string file_name;
+            std::cout << "Name a file: ";
+            std::cin >> file_name;
+            delete_file(file_name);
+        }
+        else if (command == "create text file")
+        {
+            std::string file_name;
+            std::cout << "Enter file name to create: ";
+            std::cin >> file_name;
+            create_file(file_name);
+        }
+        else if (command == "read file")
+        {
+            std::string file_name;
+            std::cout << "Enter file name to read: ";
+            std::cin >> file_name;
+            read_file(file_name);
+        }
+        else
+        {
+            std::cout << "That is not a valid command" << std::endl;
+        }
+    }
 
-	while(true)
-	{
-		string eksla;
-		std::cout << "Enter Command:"
-		std::cin >> eksla
-		if (eksla == "delete file")
-		{
-			string file_name;
-			std::cout << "Name a file: ";
-			std::cin << file_name;
-			delete_file(file_name);
-		}
-		if (eksla == "create file")
-		{
-			std::cin << "not yet added";
-		}
-		else
-		{
-			std::cin << "that is not a cmd";
-		}
-
-		return 0;
-	}
-
-	return 1;
+    return 1;
 }
